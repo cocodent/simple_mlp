20218ad9f44e85fcce59e0f81de5b2a7274be44a
coco <coco.suteru@gmail.com>
2019-02-14 21:28:25 +0900

    initial commit

---
+++ backward_3_layers_xor.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+const auto INPUT = 2;
+const auto HIDDEN = 30;
+const auto LR = 0.01;
+const auto EPOCH = 1000000;
+
+
+double sigmoid(double x) {
+  return 1 / ( 1 + exp(-x));
+}
+
+constexpr double d_sigmoid(double x) {
+  return x * (1 - x);
+}
+
+vector<vector<double>>matmul(vector<vector<double>> vv1, vector<vector<double>> vv2){
+  int i, j ,k;
+  double temp;
+
+  const size_t vv1_row = vv1.size();
+  const size_t vv1_column = vv1[0].size();
+  if(vv1_column!=vv2.size()) cout << "error" << endl;
+  const size_t vv2_column = vv2[0].size();
+
+  vector<vector<double>>result (vv1_row, vector<double>(vv2_column));
+
+  for(i=0; i<vv1_row; i++){
+    for(j=0; j<vv2_column; j++){
+      temp = 0;
+      for(k=0; k<vv1_column; k++){
+        temp += vv1[i][k] * vv2[k][j];
+      }
+      result[i][j] = sigmoid(temp);
+    }
+  }
+  return result;
+}
+
+
+int main(){
+    int i, j, k, l;
+    double temp;
+
+    //XOR train data
+    vector<vector<double>>train_x = {{0,0},{0,1},{1,0},{1,1}};
+    vector<double>train_y = {0,1,1,0};
+
+    //network
+    vector<vector<double>>w1 (INPUT, vector<double>(HIDDEN));
+    vector<double>w2 (HIDDEN);
+    vector<vector<double>>o1 (4, vector<double>(HIDDEN));
+    vector<double>output (4);
+
+    //initialize weights with random value
+    for(i=0; i<INPUT; i++){
+      for(j=0; j<HIDDEN; j++){
+        w1[i][j] = (double)rand() / (double)RAND_MAX;
+      }
+    }
+
+    for(i=0; i<HIDDEN; i++){
+      w2[i] = (double)rand() / (double)RAND_MAX;
+    }
+
+    //train
+    cout << "OUTPUT" << endl;
+
+    for(l=0; l<EPOCH+1; l++){
+      if(l%100000==0) {
+        for(i=0; i<4; i++){
+          cout << fixed << output[i] << ' ';
+        }
+        cout << " / EPOCH : " << l << endl;
+      }
+
+      //forward
+      //input layer
+      o1 = matmul(train_x, w1);
+
+      //hidden layer
+      for(i=0; i<4; i++){
+          temp = 0;
+          for(k=0; k<HIDDEN; k++){
+            temp += o1[i][k] * w2[k];
+          }
+        output[i] = sigmoid(temp);
+        }
+
+      //backword
+      //hidden layer
+      for(i=0; i<4; i++){
+        for(j=0; j<HIDDEN; j++){
+            w2[j] -= LR * o1[i][j] * d_sigmoid(output[i]) * (output[i] - train_y[i]);
+        }
+      }
+      //input layer
+      for(i=0; i<4; i++){
+        for(j=0; j<INPUT; j++){
+          for(k=0; k<HIDDEN; k++){
+            w1[j][k] -= LR * train_x[i][j] * d_sigmoid(o1[i][k]) * d_sigmoid(output[i]) * (output[i] - train_y[i]) * w2[k];
+          }
+        }
+      }
+    }
+    
+    //predict
+    //input XOR
+    for(i=0; i<4; i++){
+      for(j=0; j<HIDDEN; j++){
+        temp = 0;
+        for(k=0; k<INPUT; k++){
+          temp += train_x[i][k] * w1[k][j];
+        }
+      o1[i][j] = sigmoid(temp);
+      }
+    }
+    //hidden layer
+    for(i=0; i<4; i++){
+        temp = 0;
+        for(k=0; k<HIDDEN; k++){
+          temp += o1[i][k] * w2[k];
+        }
+      output[i] = sigmoid(temp);
+      }
+
+  //result
+    cout << "RESULT" << endl;
+    for(i=0; i<4; i++){
+      cout << train_x[i][0] << ' ' << train_x[i][1] << " -> ";
+      cout << output[i] << endl;
+      }
+
+  return 0;
+}
